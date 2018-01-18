d3b4d43e10df33d03c12ade5d4f78dc7cc6d88d2
Dennis Xiao <dennisboys@gmail.com>
2018-01-18 22:32:12 +1100

    improved pocket money style

--- src/components/PocketStats.js
+++ src/components/PocketStats.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const PocketStats = (props) => {
+
+  const labelStyle = props.pocketMoney >= 0 ? "label label-success" : "label label-danger";
+
   return (
     <h2 className="text-center" style={{ margin: '20px 0' }}>
-      You have <span className="label label-success">$ {props.pocketMoney}</span> to spend on grocery today!
+      You have <span className={labelStyle}>$ {props.pocketMoney}</span> to spend on grocery today!
     </h2>
   )
 }
